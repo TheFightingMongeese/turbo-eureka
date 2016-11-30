bad27369476e1e7c1a9951482184a156254e6e56
Sigga <sigga@hotmail.com>
2016-11-30 10:24:39 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
-#include "readinfo.h"
 #include <iostream>
-#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-      ReadInfo read;
+    ofstream outFile;
+
+    outFile.open("Database.txt");
+
+    outFile << "Test" << endl;
+
+    outFile.close();
 
 
-      read.run();
 
+    ifstream inFile;
 
+    inFile.open("Database.txt");
 
+    if(inFile.is_open())
+    {
+        inFile.seekg(0, inFile.end);
+        int length = inFile.tellg();
+        inFile.seekg(0, inFile.beg);
 
+        char * optrBuffer = new char[length+1];
+
+        inFile.get(optrBuffer, length);
+
+        cout << optrBuffer << endl;
+
+        delete [] optrBuffer;
+    }
+
+    return 0;
 }
