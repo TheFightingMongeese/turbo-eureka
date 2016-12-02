e87c3d133038887f609395f6a7f05118e17f5559
bleikur5 <eikibleiki55@gmail.com>
2016-12-02 15:52:07 +0000

    Add files via upload

--- database.h
+++ database.h
@@ -1,7 +1,7 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 #include "readinfo.h"
-#include <vector>
+//#include <vector>
 
 using namespace std;
 class DataBase
@@ -9,6 +9,9 @@ class DataBase
 public:
     DataBase(vector<Scientist> s, int numberOfSci);
     void makeFile();
+    void alphabet();
+    void byBirth();
+    void byDeath();
 };
 
 #endif // DATABASE_H
---
+++ displayinfo.h
@@ -0,0 +1,13 @@
+#ifndef DISPLAYINFO_H
+#define DISPLAYINFO_H
+
+
+class DisplayInfo
+{
+public:
+    DisplayInfo();
+
+    void display();
+};
+
+#endif // DISPLAYINFO_H
