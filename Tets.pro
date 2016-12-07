d96349ec406f90a5e80c306cf868392f67ca4d5d
OlafurAO <olafurao16@ru.is>
2016-12-07 12:34:11 +0000

    Add files via upload

---
+++ Tets.pro
@@ -0,0 +1,13 @@
+QT += core
+QT -= gui
+QT += core sql
+
+CONFIG += c++11
+
+TARGET = Tets
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
--- main.cpp
+++ main.cpp
@@ -1,14 +1,47 @@
+#include <iostream>
+#include <string>
+#include <QCoreApplication>
+#include <QtSql>
+#include <QtDebug>
+#include <QString>
 
-#include "database.h"
+using namespace std;
 
 int main()
 {
-    vector<Scientist> s;
-    //DataBase make;
-    ReadInfo read;
+    QSqlDatabase db;
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    QString dbName = "Computers.sqlite";
+    db.setDatabaseName(dbName);
 
-    //make.makeFile();
-    s = read.run();
-    DataBase b(s, read.getNumberOfSci());
+  db.open();
+
+
+  QSqlQuery query(db);
+
+
+  query.exec("SELECT * FROM Computers");
+
+
+
+  while(query.next())
+  {
+           int year = query.value("Year").toUInt();
+           string name = query.value("Name").toString().toStdString();
+           bool built = query.value("Built").toUInt();
+           string type = query.value("Type").toString().toStdString();
+  }
+
+
+
+
+
+
+
+
+
+//  computers.push_back(Computers(year, name, built, type));
+
+
 
 }
